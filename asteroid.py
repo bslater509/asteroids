015cd3b262072ffea9bf3cb29d359031eda3cf11
bslater509 <bslater509@gmail.com>
2026-01-02 21:20:40 +0000

    4.1.2 Creating Constructor for asteroid

---
+++ asteroid.py
@@ -0,0 +1,5 @@
+
+class Asteroid(CircleShape):
+    def __init__(self, x,y,radius):
+        super().__init__(x,y,radius)
+    def draw(self):
--- main.py
+++ main.py
@@ -20,18 +20,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
-
+    #game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
+        #update and draw all sprites
         updatable.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
-
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
